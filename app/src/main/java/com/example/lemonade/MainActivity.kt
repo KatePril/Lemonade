1d33a8cd23f38270960b5a0a960417487c4d05bf
KatePril <ket14pril14@gmail.com>
2024-02-01 16:01:35 +0200

    Update functionality

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -11,13 +11,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,6 +36,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,51 +56,94 @@ class MainActivity : ComponentActivity() {
 )
 @Composable
 fun LemonadeApp() {
-    LemonadeWithButtonAndImage(modifier = Modifier
+    AppStep(modifier = Modifier
         .fillMaxSize()
         .wrapContentSize(Alignment.Center)
     )
 }
 
 @Composable
-fun LemonadeWithButtonAndImage(modifier: Modifier = Modifier) {
+fun AppStep(modifier: Modifier = Modifier) {
     var step by remember { mutableStateOf(1) }
-    val imageResource = when (step) {
-        1 -> R.drawable.lemon_tree
-        in 2..6 -> R.drawable.lemon_squeeze
-        in 7..11 -> R.drawable.lemon_drink
-        else -> R.drawable.lemon_restart
-    }
-    val titleResource = when (step) {
-        1 -> R.string.img_title_one
-        in 2..6 -> R.string.img_title_two
-        in 7..11 -> R.string.img_title_three
-        else -> R.string.img_title_four
-    }
-    val textResource = when (step) {
-        1 -> R.string.msg_step_one
-        in 2..6 -> R.string.msg_step_two
-        in 7..11 -> R.string.msg_step_three
-        else -> R.string.msg_step_four
-    }
+    var squeezeCount by remember { mutableStateOf(0) }
 
+    Scaffold { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when (step) {
+                1 -> {
+                    LemonadeWithButtonAndImage(
+                        imageId = R.drawable.lemon_tree,
+                        titleId = R.string.img_title_one,
+                        textId = R.string.msg_step_one,
+                        onImageCLick = {
+                            step = 2
+                            squeezeCount = (2..6).random()
+                        }
+                    )
+                }
+                2 -> {
+                    LemonadeWithButtonAndImage(
+                        imageId = R.drawable.lemon_squeeze,
+                        titleId = R.string.img_title_two,
+                        textId = R.string.msg_step_two,
+                        onImageCLick = {
+                            squeezeCount--
+                            if (squeezeCount == 0) step = 3
+                        }
+                    )
+                }
+                3 -> {
+                    LemonadeWithButtonAndImage(
+                        imageId = R.drawable.lemon_drink,
+                        titleId = R.string.img_title_three,
+                        textId = R.string.msg_step_three,
+                        onImageCLick = { step = 4 }
+                    )
+                }
+                4 -> {
+                    LemonadeWithButtonAndImage(
+                        imageId = R.drawable.lemon_restart,
+                        titleId = R.string.img_title_four,
+                        textId = R.string.msg_step_four,
+                        onImageCLick = { step = 1 }
+                    )
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun LemonadeWithButtonAndImage(
+    imageId: Int,
+    titleId: Int,
+    textId: Int,
+    onImageCLick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(
-            onClick = { step++ },
+            onClick = onImageCLick,
+
         ) {
             Image(
-                painter = painterResource(imageResource),
-                contentDescription = stringResource(titleResource)
+                painter = painterResource(imageId),
+                contentDescription = stringResource(titleId)
             )
         }
         Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = stringResource(textResource),
+            text = stringResource(textId),
             fontSize = 16.sp
         )
     }
